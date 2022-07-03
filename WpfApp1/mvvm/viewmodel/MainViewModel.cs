845abd7cc18fe27cc7e572dc5de1c722371de168
parmisvz <parmisvaziferm@gmail.com>
2022-07-04 00:06:36 +0430

    Added: AdminDashboard - Deleted: All usercontrols and extra files

--- WpfApp1/mvvm/viewmodel/MainViewModel.cs
+++ WpfApp1/mvvm/viewmodel/MainViewModel.cs
@@ -9,11 +9,8 @@ using WpfApp1.mvvm.viemodel;
 namespace WpfApp1.mvvm.viewmodel
 {
      class MainViewModel:observableobject
-    {
-        public RelayCommand HomeViewCommand { get; set; }
-        public RelayCommand ProfileViewCommand { get; set; }
+     {      
         public HomeViewModel HomeVm { get; set; }
-        public Profileviewmodel proVm { get; set; }
         private object _currentView;
         public object CurrentView
         {
@@ -24,16 +21,7 @@ namespace WpfApp1.mvvm.viewmodel
         public MainViewModel()
         {
             HomeVm=new HomeViewModel();
-            proVm=new Profileviewmodel();
             CurrentView = HomeVm;
-            HomeViewCommand = new RelayCommand(o =>
-              {
-                  CurrentView = HomeVm;
-              });
-            ProfileViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = proVm;
-            });
 
         }
     }
